caf645b006594e918315d41fb30da37cfb6b27a0
Ramana <ramanakellampalli@gmail.com>
2017-09-21 18:29:19 -0400

    Added Alerts and toasts for the edit-recipe component

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActionSheetController, AlertController, NavParams} from "ionic-angular";
+import {ActionSheetController, AlertController, NavParams, ToastController} from "ionic-angular";
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -13,7 +13,8 @@ export class EditRecipePage implements OnInit {
 
   constructor (private navParams: NavParams,
                private actionSheetController: ActionSheetController,
-               private alertCtrl: AlertController) {}
+               private alertCtrl: AlertController,
+               private toastCtrl: ToastController) {}
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
@@ -31,12 +32,26 @@ export class EditRecipePage implements OnInit {
         {
           text:'Add Ingredients',
           handler: () => {
+            this.createNewIngredientAlert().present();
           }
         },
         {
           text: 'Remove all Ingredients',
           role: 'destructive',
           handler: () => {
+            const fArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
+            const len = fArray.length;
+            if (len > 0) {
+              for (let i = len - 1; i >= 0; i--) {
+                fArray.removeAt(i);
+              }
+              const toast = this.toastCtrl.create({
+                message: 'All Ingredients were deleted!',
+                duration: 1500,
+                position: 'bottom'
+              });
+              toast.present();
+            }
           }
         },
         {
@@ -45,10 +60,11 @@ export class EditRecipePage implements OnInit {
         }
       ]
     });
+    actionSheet.present();
   }
 
   private createNewIngredientAlert() {
-    const newIngredientAlert = this.alertCtrl.create({
+    return this.alertCtrl.create({
       title: 'Add Ingredient',
       inputs: [
         {
@@ -65,8 +81,22 @@ export class EditRecipePage implements OnInit {
           text: 'Add',
           handler: data => {
             if (data.name.trim() == '' || data.name == null) {
+              const toast = this.toastCtrl.create({
+                message: 'Please enter a valid value!',
+                duration: 1500,
+                position: 'bottom'
+              });
+              toast.present();
+              return;
             }
-
+            (<FormArray>this.recipeForm.get('ingredients')).push(new FormControl(data.name,
+            Validators.required));
+            const toast = this.toastCtrl.create({
+              message: 'Item added!',
+              duration: 1500,
+              position: 'bottom'
+            });
+            toast.present();
           }
         }
       ]
