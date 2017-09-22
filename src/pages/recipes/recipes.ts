60b33668c5043c88e597e4f3556cd78de03d34b6
Ramana <ramanakellampalli@gmail.com>
2017-09-22 19:14:48 -0400

    Updated Reicpe page after hitting the Add Recipe button

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -2,16 +2,24 @@ import { Component } from '@angular/core';
 import {NavController} from "ionic-angular";
 
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {Recipe} from "../../models/recipe";
+import {RecipesService} from "../services/recipes";
 
 @Component({
   selector: 'page-recipes',
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
+  recipes: Recipe[];
+  constructor (private navCtrl: NavController, private recipesService: RecipesService) {}
 
-  constructor (private navCtrl: NavController) {}
+  ionViewWillEnter() {
+    this.recipes = this.recipesService.getRecipes();
+  }
 
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'New'});
   }
+
+  onLoadRecipe() {}
 }
--- src/pages/services/recipes.ts
+++ src/pages/services/recipes.ts
@@ -10,6 +10,7 @@ export class RecipesService {
             difficulty: string,
             ingredients: Ingredient[]) {
     this.recipes.push(new Recipe(title, description, difficulty, ingredients));
+    console.log(this.recipes);
   }
   getRecipes() {
     return this.recipes.slice();
